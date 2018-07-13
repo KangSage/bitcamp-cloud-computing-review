95e5626d269b6ca64feb73eaad58daf68d4185e0
KangSage <ksage@knou.ac.kr>
2018-07-13 21:39:15 +0900

    201.07.13_21:40 오늘 마지막 연습

--- bitcamp-spring-ioc/src/main/java/bitcamp/cloud/step04/Exam01.java
+++ bitcamp-spring-ioc/src/main/java/bitcamp/cloud/step04/Exam01.java
@@ -4,16 +4,17 @@ package bitcamp.cloud.step04;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import bitcamp.cloud.BeanUtils;
-
 public class Exam01 {
     public static void main(String[] args) {
         ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
                 "bitcamp/cloud/step04/application-context-01.xml");
         
         System.out.println(iocContainer.getBean("c1"));
+        System.out.println("========================");
         System.out.println(iocContainer.getBean("c2"));
+        System.out.println("========================");
         System.out.println(iocContainer.getBean("c3"));
+        System.out.println("========================");
         System.out.println(iocContainer.getBean("c4"));
 
     }
