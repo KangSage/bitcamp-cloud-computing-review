f61c8087a6e6527de72a2d5e4841150f8f2d04ba
KangSage <ksage@knou.ac.kr>
2018-08-04 10:52:38 +0900

    2018.08.04_10:51 폰갭 과제 및 복습

---
+++ bitcamp-phonegap-app01-review/www/game/tictectoe03.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$('.cell').click((e) => {
+    console.log($(e.target).attr('data-pos'));
+});
+
+$('#new-game').click((e) => {
+    console.log('new game!');
+});
---
+++ bitcamp-phonegap-app01-review/www/js/common.js
@@ -0,0 +1,4 @@
+// 서버를 시작한 후 IP와 홈 주소를 확인해서 변수에 넣어줄 것.
+
+var eclipseServerApiAddr = "http://localhost:8080/bitcamp-web-18";
+var ideaServerApiAddr = "http://192.168.0.73:8080/bitcamp-web-18";
